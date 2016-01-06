0b4d5f058fd51ed7b78a17cd8d0e7860015f9280
Ashley <ashley@victorianfox.com>
2016-01-06 20:49:36 +0100

    Take care of FoxLog trying to get MODID in coremod stage returning NULL

--- src/main/java/foxie/lib/FoxLog.java
+++ src/main/java/foxie/lib/FoxLog.java
@@ -5,20 +5,27 @@ import net.minecraftforge.fml.common.FMLLog;
 
 public class FoxLog {
    public static void warning(String msg) {
-      FMLLog.warning("[" + Things.getCurrentModId() + "] " + msg);
+      FMLLog.warning("[" + getModid() + "] " + msg);
    }
 
    public static void error(String msg) {
-      FMLLog.severe("[" + Things.getCurrentModId() + "] " + msg);
+      FMLLog.severe("[" + getModid() + "] " + msg);
    }
 
    public static void info(String msg) {
-      FMLLog.severe("[" + Things.getCurrentModId() + "] " + msg);
+      FMLLog.severe("[" + getModid() + "] " + msg);
    }
 
    public static void notAFoxie() {
       error("    Fake Foxie detected! This is not a proper Foxie!");
       error("<-- just tried working with a mod that uses FoxieLib but does not implement IFoxieMod!");
       error("    report this to the author of given mod, please. Thanks!");
+   }
+
+   private static String getModid() {
+      if (Things.getCurrentModId() == null) // modid is not available during startup
+         return "FoxieCoreMod";
+
+      return Things.getCurrentModId();
    }
 }
